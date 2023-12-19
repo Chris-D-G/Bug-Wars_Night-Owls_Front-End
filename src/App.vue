40073ce31b45f44fed86bf39686eea2db9252aa4
MicahMcLain <mclainmicah1@gmail.com>
2023-12-18 23:51:21 -0500

    styled most of the home screen. added a few bug icons to make the page less blank.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="bg-gray-500 h-screen" id="capstone-app">
+  <div class="h-screen" id="capstone-app">
     <div id="nav">
-      <router-link
-        class="text-white font-bold py-2 px-4 ml-3 rounded"
-        v-bind:to="{ name: 'home' }"
-        >Home</router-link
-      >&nbsp;|&nbsp;
-      <router-link
-        class="text-white font-bold py-2 px-4 rounded"
-        v-bind:to="{ name: 'login' }"
-        v-if="$store.state.token == ''"
-        >Login</router-link
+      <button
+        class="bg-gray-400 text-white font-bold py-2 px-4 ml-3 rounded-3xl"
       >
-      <router-link
-        class="text-white font-bold py-2 px-4 rounded"
-        v-bind:to="{ name: 'logout' }"
-        v-if="$store.state.token != ''"
-        >Logout</router-link
-      >
+        <router-link v-bind:to="{ name: 'home' }">Home</router-link>
+      </button>
+      &nbsp;|&nbsp;
+      <button class="bg-gray-400 text-white font-bold py-2 px-4 rounded-3xl">
+        <router-link
+          v-bind:to="{ name: 'login' }"
+          v-if="$store.state.token == ''"
+          >Login</router-link
+        >
+      </button>
+      <button>
+        <router-link
+          class="text-white font-bold py-2 px-4 rounded"
+          v-bind:to="{ name: 'logout' }"
+          v-if="$store.state.token != ''"
+          >Logout</router-link
+        >
+      </button>
     </div>
     <router-view />
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,54 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <!-- this sets the text to 8xl, which is 6rem. The rest of the css creates a gradient background, makes the text transparent, and the clips the background. Font family is set in style sheet below.   -->
+    <h1
+      class="text-8xl bg-gradient-to-r from-green-700 via-gray-800 to-emerald-400 inline-block text-transparent bg-clip-text"
+    >
+      Bug Wars
+    </h1>
+    <p class="text-center text-2xl">
+      Please wait... the <a class="underline decoration-sky-500">bugs</a> have
+      taken over and our website isn't done yet
+    </p>
+  </div>
+  <div class="flex flex-row justify-center max-w mt-16">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      data-slot="icon"
+      class="w-2/5 h-2/5 rotate-45"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12 12.75c1.148 0 2.278.08 3.383.237 1.037.146 1.866.966 1.866 2.013 0 3.728-2.35 6.75-5.25 6.75S6.75 18.728 6.75 15c0-1.046.83-1.867 1.866-2.013A24.204 24.204 0 0 1 12 12.75Zm0 0c2.883 0 5.647.508 8.207 1.44a23.91 23.91 0 0 1-1.152 6.06M12 12.75c-2.883 0-5.647.508-8.208 1.44.125 2.104.52 4.136 1.153 6.06M12 12.75a2.25 2.25 0 0 0 2.248-2.354M12 12.75a2.25 2.25 0 0 1-2.248-2.354M12 8.25c.995 0 1.971-.08 2.922-.236.403-.066.74-.358.795-.762a3.778 3.778 0 0 0-.399-2.25M12 8.25c-.995 0-1.97-.08-2.922-.236-.402-.066-.74-.358-.795-.762a3.734 3.734 0 0 1 .4-2.253M12 8.25a2.25 2.25 0 0 0-2.248 2.146M12 8.25a2.25 2.25 0 0 1 2.248 2.146M8.683 5a6.032 6.032 0 0 1-1.155-1.002c.07-.63.27-1.222.574-1.747m.581 2.749A3.75 3.75 0 0 1 15.318 5m0 0c.427-.283.815-.62 1.155-.999a4.471 4.471 0 0 0-.575-1.752M4.921 6a24.048 24.048 0 0 0-.392 3.314c1.668.546 3.416.914 5.223 1.082M19.08 6c.205 1.08.337 2.187.392 3.314a23.882 23.882 0 0 1-5.223 1.082"
+      />
+    </svg>
+
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-2/5 h-2/5 -rotate-45"
+      viewBox="0 0 512 512"
+    >
+      <path
+        d="M256 0c53 0 96 43 96 96v3.6c0 15.7-12.7 28.4-28.4 28.4H188.4c-15.7 0-28.4-12.7-28.4-28.4V96c0-53 43-96 96-96zM41.4 105.4c12.5-12.5 32.8-12.5 45.3 0l64 64c.7 .7 1.3 1.4 1.9 2.1c14.2-7.3 30.4-11.4 47.5-11.4H312c17.1 0 33.2 4.1 47.5 11.4c.6-.7 1.2-1.4 1.9-2.1l64-64c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3l-64 64c-.7 .7-1.4 1.3-2.1 1.9c6.2 12 10.1 25.3 11.1 39.5H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H416c0 24.6-5.5 47.8-15.4 68.6c2.2 1.3 4.2 2.9 6 4.8l64 64c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0l-63.1-63.1c-24.5 21.8-55.8 36.2-90.3 39.6V240c0-8.8-7.2-16-16-16s-16 7.2-16 16V479.2c-34.5-3.4-65.8-17.8-90.3-39.6L86.6 502.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l64-64c1.9-1.9 3.9-3.4 6-4.8C101.5 367.8 96 344.6 96 320H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H96.3c1.1-14.1 5-27.5 11.1-39.5c-.7-.6-1.4-1.2-2.1-1.9l-64-64c-12.5-12.5-12.5-32.8 0-45.3z"
+      />
+    </svg>
   </div>
 </template>
 
 <script>
-export default {
-};
+export default {};
 </script>
+<style>
+h1 {
+  font-family: "Silkscreen", sans-serif;
+  text-align: center;
+}
+p {
+  font-family: "Orbitron", sans-serif;
+}
+</style>
